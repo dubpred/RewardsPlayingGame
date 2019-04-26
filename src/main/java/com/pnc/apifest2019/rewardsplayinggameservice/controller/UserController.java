235f6e435fbd9d5527df44672ef7177bc8727591
Adam <adamvc2121@gmail.com>
2019-04-26 08:21:19 +0000

    cross-origin

--- src/main/java/com/pnc/apifest2019/rewardsplayinggameservice/controller/UserController.java
+++ src/main/java/com/pnc/apifest2019/rewardsplayinggameservice/controller/UserController.java
@@ -7,12 +7,7 @@ import com.pnc.apifest2019.rewardsplayinggameservice.model.dto.response.UserResp
 import com.pnc.apifest2019.rewardsplayinggameservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -35,6 +30,7 @@ public class UserController {
         return ResponseEntity.ok(userService.createUser(createUserDto));
     }
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<DemoResponseDto> getDemo (@RequestParam long userId){
         return ResponseEntity.ok(userService.getDemo(userId));
